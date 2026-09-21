8be930e7484201f30c7948216e741ad4803ae351
carloscabral656 <edu.carlos.cec@gmail.com>
2026-09-21 14:09:42 -0300

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HomePage } from './slices/home/page/home-page.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomePage],
+  imports: [HeaderComponent, HomePage, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { IconButton } from '../icon-button/icon-button.component';
+
+@Component({
+  selector: 'footer-component',
+  imports: [],
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {}
